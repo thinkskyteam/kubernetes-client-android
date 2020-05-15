543fdf7bee30e2ed38fc8cff366cda1f85ce51a1
Navid Eivazzadeh <navid.ei2008@gmail.com>
2020-05-15 21:14:40 +0430

    KKC-2: Replace wildcard imports

--- kubernetes-client/src/main/java/io/fabric8/kubernetes/client/Config.java
+++ kubernetes-client/src/main/java/io/fabric8/kubernetes/client/Config.java
@@ -37,7 +37,12 @@ import okhttp3.TlsVersion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
